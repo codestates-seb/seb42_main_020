babd79e1b65515f456f107932f335454d37a443e
Mia-Oh <songmiaoh@gmail.com>
2023-03-07 18:56:26 +0900

    init:clinet-setting #3

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { RecoilRoot } from 'recoil';
+import ExPage from './Pages/ExPage';
+import './variables.css';
+
+function App() {
+  return (
+    <RecoilRoot>
+      <ExPage />
+    </RecoilRoot>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/ExPage.jsx
@@ -0,0 +1,15 @@
+import CharacterCounter from '../Components/CharacterCounter';
+import Menu from '../Components/Menu';
+import { DatePicker } from 'antd';
+
+function ExPage() {
+  return (
+    <>
+      <CharacterCounter />
+      <DatePicker />
+      <Menu />
+    </>
+  );
+}
+
+export default ExPage;
