768de34b9e3b5699ce569d3bbf33211c87055ffc
Mia-Oh <songmiaoh@gmail.com>
2023-03-08 15:32:22 +0900

    chore:라이브러리 설치 테스트 코드 초기화 #3

--- client/src/Style/MenuStyle.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Button } from 'antd';
-
-export const MenuButton = styled(Button)`
-  background-color: var(--mint-600);
-`;
-
-// dear.뿌런트
-// style은 js 파일로 작성했습니다
