c20cb8131ccf707bb2a6318ba3b7dbe02d4ceeac
Mia-Oh <songmiaoh@gmail.com>
2023-03-09 11:12:06 +0900

    feat:Login 페이지-기본적인 요소 나열 #16

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,5 +1,57 @@
+import styled from 'styled-components';
+
+const SMain = styled.main``;
+
+const SLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SInfoSection = styled.div``;
+
+const SFormSection = styled.div``;
+
+const SInput = styled.input``;
+
+const SSubmitBtn = styled.button``;
+
+const SGoogleLoginBtn = styled.button``;
+
+const SSignupInfo = styled.div`
+  border: 1px soild black;
+`;
+
+const SSignupBtn = styled.button``;
+
 function Login() {
-  return <></>;
+  return (
+    <SMain>
+      <SLayout>
+        <SInfoSection>
+          <img src="images/logo.png" alt="logo" />
+          <h1>로그인</h1>
+          <p>로그인으로 다나아의 다양한 서비스를 경험해 보세요</p>
+        </SInfoSection>
+        <SFormSection>
+          <SInput placeholder="이메일" />
+          <SInput placeholder="비밀번호" />
+          <div>
+            {/* <Link to="/index.html"> */}
+            <SSubmitBtn>로그인</SSubmitBtn>
+            {/* </Link> */}
+          </div>
+          <SGoogleLoginBtn>
+            <span>구글로 시작하기</span>
+          </SGoogleLoginBtn>
+        </SFormSection>
+        <SSignupInfo>
+          <p>다나아 시작하기</p>
+          <SSignupBtn>회원 가입</SSignupBtn>
+        </SSignupInfo>
+      </SLayout>
+    </SMain>
+  );
 }
 
 export default Login;
