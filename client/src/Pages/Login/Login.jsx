372e77b58b96f440f9e7aeadaef1a5b3f2b10f5f
Mia-Oh <songmiaoh@gmail.com>
2023-03-09 17:24:35 +0900

    feat:Login-유효성 검사 추가 및 상단 알림 구현 #16

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,11 +1,9 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { FcGoogle } from 'react-icons/fc';
+import { message } from 'antd';
 
 const SMain = styled.main`
-  @font-face {
-    font-family: 'TheJamsil5Bold';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302_01@1.0/TheJamsil5Bold.woff2')
-      format('woff2');
-  }
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -84,6 +82,10 @@ const SSubmitBtn = styled.button`
   width: 100%;
   height: 2.8rem;
   margin: 20px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
   background-color: var(--blue-200);
   border: 1px solid var(--blue-200);
   box-shadow: 0 1px 3px 0 var(--gray-200);
@@ -94,6 +96,10 @@ const SSubmitBtn = styled.button`
 const SGoogleLoginBtn = styled(SSubmitBtn)`
   background-color: var(--gray-100);
   border: 1px solid var(--gray-200);
+  svg {
+    font-size: 25px;
+    margin: 0 5px 0 0;
+  }
 `;
 
 const SSignupInfo = styled.div`
@@ -108,6 +114,12 @@ const SSignupInfo = styled.div`
     font-weight: 500;
     font-size: 1rem;
   }
+  p {
+    font-family: 'TheJamsil';
+    font-weight: 400;
+    font-size: 1rem;
+    color: var(--gray-800);
+  }
 `;
 
 const SSignupBtn = styled(SSubmitBtn)`
@@ -117,23 +129,95 @@ const SSignupBtn = styled(SSubmitBtn)`
 `;
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [messageApi, contextHolder] = message.useMessage();
+  const [emailMsg, setEmailMsg] = useState(''); // 유효성 검사 안내 Msg for eamil
+  const [passwordMsg, setPasswordMsg] = useState(''); // 유효성 검사 안내 Msg for PW
+
+  console.log(passwordMsg);
+
+  const notTobeNull = ({ email, password }) => {
+    return email !== null && password !== null;
+  };
+
+  // 이메일 정규 표현식
+  const validateEmail = (email) => {
+    return email
+      .toLowerCase()
+      .match(
+        /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+      );
+  };
+
+  // 비밀번호 정규 표현식
+  // 문자, 특수문자, 숫자를 포함 (글자 수 최소 8 ~ 최대 15)
+  const validatePwd = (password) => {
+    return password
+      .toLowerCase()
+      .match(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/);
+  };
+
+  // 이메일
+  const handleChangeEmail = (e) => {
+    const currEmail = e.target.value;
+
+    setEmail(currEmail);
+    if (!validateEmail(email)) {
+      setEmailMsg(`${currEmail} 은 올바른 이메일 형식이 아닙니다.`);
+    } else {
+      setEmailMsg('');
+    }
+  };
+
+  // 비밀번호
+  const handleChangePassword = (e) => {
+    const currPassword = e.target.value;
+
+    setPassword(currPassword);
+
+    if (!validatePwd(password)) {
+      setPasswordMsg(`비밀번호를 확인하여 주십시오.`);
+    } else {
+      setPasswordMsg('');
+    }
+  };
+
+  // 유효성 검사 미통과 안내 Msg
+  const handleClickAlert = () => {
+    messageApi.open({
+      type: 'warning',
+      content: emailMsg || passwordMsg || '이메일과 비밀번호를 입력해 주세요',
+    });
+    console.log(isNotNull);
+  };
+
+  // 유효성 검사를 통과하지 못하면 Submit 비활성화
+  const isEmailValid = validateEmail(email);
+  const isPwdValid = validatePwd(password);
+  const isNotNull = notTobeNull({ email, password });
+  const isAllValid = isEmailValid && isPwdValid && isNotNull;
+
   return (
     <SMain>
       <SLayout>
+        {isAllValid ? null : contextHolder}
         <SInfoSection>
           <img src="images/logo.png" alt="logo" />
           <h1>로그인</h1>
           <p>로그인으로 다나아의 다양한 서비스를 경험해 보세요</p>
         </SInfoSection>
         <SFormSection>
-          <SInput placeholder="이메일" />
-          <SInput placeholder="비밀번호" />
+          <SInput onChange={handleChangeEmail} placeholder="이메일" />
+          <SInput onChange={handleChangePassword} placeholder="비밀번호" />
           <div>
             {/* <Link to="/index.html"> */}
-            <SSubmitBtn>로그인</SSubmitBtn>
+            <SSubmitBtn onClick={handleClickAlert}>로그인</SSubmitBtn>
             {/* </Link> */}
           </div>
           <SGoogleLoginBtn>
+            <FcGoogle />
             <span>구글로 시작하기</span>
           </SGoogleLoginBtn>
         </SFormSection>
