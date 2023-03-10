30167c1d533d52645b86e7f9e5f17c73b98fa6d4
suming <ipower_overi@naver.com>
2023-03-10 21:20:40 +0900

    feat: Nav Component router Link 설정 #8

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,24 +1,25 @@
 import { SNavContainer, SNavBlock, SNavContents } from '../../Style/NavStyle';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <SNavContainer>
       <SNavBlock>
         <SNavContents>
-          <a href="/all">전체</a>
+          // 자세한 링크는 수정 예정
+          <Link href="/all">전체</Link>
         </SNavContents>
         <SNavContents>
-          <a href="/questions">질문</a>
+          <Link to="/askquestion">질문</Link>
         </SNavContents>
         <SNavContents>
-          <a href="/reviews">리뷰</a>
+          <Link href="/reviews">리뷰</Link>
         </SNavContents>
         <SNavContents>
-          <a href="/locations">지역별</a>
+          <Link href="/locations">지역별</Link>
         </SNavContents>
         <SNavContents>
-          <a href="/post">진료 과목</a>
+          <Link href="/post">진료 과목</Link>
         </SNavContents>
       </SNavBlock>
     </SNavContainer>
