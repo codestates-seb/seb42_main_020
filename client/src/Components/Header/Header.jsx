3383355726452cd84d2c068d960b5cc6aff4db7d
Mia-Oh <songmiaoh@gmail.com>
2023-03-17 01:04:16 +0900

    feat:로그인 여부, 로그인 유저 정보 상태 추가 #15

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 import Nav from '../Nav/Nav';
 import { Popover, Avatar, Badge } from 'antd';
 import { UserOutlined, BellOutlined } from '@ant-design/icons';
@@ -14,18 +15,32 @@ import {
   SMyInfo,
   SLogout,
 } from '../../Style/HeaderStyle';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { loginState, loggedUserInfo } from '../../atoms/atoms';
 
 function Header() {
-  const [isLogged, setIsLogged] = useState(false); // 로그인 여부
-  const [isNoticed, setIsNoticed] = useState(true); // 답변 알림 유무
+  const [isLogged, setIsLogged] = useRecoilState(loginState); // 로그인 여부
+  const userInfo = useRecoilValue(loggedUserInfo);
+
+  const [isNoticed, setIsNoticed] = useState(false); // 답변 알림 유무
+
+  const cookies = new Cookies();
+  const navigate = useNavigate();
 
   const [isOpenNav, setIsOpenNav] = useState(false);
 
-  console.log(setIsLogged); // 로그인 여부 ESLint 오류 발생 방지 콘솔
   console.log(setIsNoticed); // 답변 알림 미사용으로 인한 ESLint 오류 발생 방지 콘솔
 
   const handleClickNav = () => {
     setIsOpenNav(!isOpenNav);
+  };
+
+  const handleClickLogout = (e) => {
+    e.preventDefault();
+    setIsLogged(!isLogged);
+    localStorage.removeItem('accessToken');
+    cookies.remove('refreshToken');
+    navigate('/home');
   };
 
   return (
@@ -58,14 +73,14 @@ function Header() {
                     <BellOutlined />
                   </SNotification>
                   <Popover
-                    title="김원필 님, 안녕하세요"
+                    title={`${userInfo?.name} 님, 안녕하세요`}
                     content={<Link to="/myinfo">마이 페이지</Link>}
                   >
                     <Avatar size="large" icon={<UserOutlined />} />
                   </Popover>
                 </SMyInfo>
                 <SLogout>
-                  <button>Log out</button>
+                  <button onClick={handleClickLogout}>Log out</button>
                 </SLogout>
               </SPrivateContent>
             )}
