ed789ff0d29d13e2696876ab3455a8b804ad334c
Mia-Oh <songmiaoh@gmail.com>
2023-03-17 01:07:29 +0900

    style:atoms 주석 추가

--- client/src/atoms/atoms.js
+++ client/src/atoms/atoms.js
@@ -1,10 +1,12 @@
 import { atom } from 'recoil';
 
+// 로그인 상태
 export const loginState = atom({
   key: 'loginState',
   default: false,
 });
 
+// 로그인한 유저 정보
 export const loggedUserInfo = atom({
   key: 'loggedUserInfo',
   default: null,
