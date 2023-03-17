195d0e1f6be0ca114ec3e000df514af2ea10a400
suming <ipower_overi@naver.com>
2023-03-17 16:09:02 +0900

    feat: 로그인 상태를 이용하여 리뷰 상세 페이지 권한 부여 및 리뷰 상세페이지 정보를 서버에서 받아와 렌더링 하는 기능 구현 #60

--- client/src/Pages/ReviewDetail/ReviewDetail.jsx
+++ client/src/Pages/ReviewDetail/ReviewDetail.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useRecoilState } from 'recoil';
 import { useNavigate } from 'react-router-dom';
+import { loginState, loggedUserInfo } from '../../atoms/atoms';
+// import { useNavigate } from 'react-router-dom';
+import CommentForm from '../../Components/CommentForm/CommentForm';
 import ReportModal from '../../Components/ReportModal/ReportModal';
 import {
   SReviewDetailContainer,
@@ -13,30 +18,47 @@ import {
 
 const ReviewDetail = () => {
   const navigate = useNavigate();
-  // 유저 확인
-  const [isWriteReview, setIsWriteReview] = useState(true);
+  // 로그인 상태 정보 확인
+  const [isLogin, setIsLogin] = useRecoilState(loginState);
+  const [loginInfo, setLoginInfo] = useRecoilState(loggedUserInfo);
+
   // 좋아요 상태관리
   const [like, setLike] = useState(false);
   // 신고 모달 관리
   const [reportModal, setReportModal] = useState(true);
+  // 받아오는 정보 관리
+  const [reviewData, setReviewData] = useState({});
+  // 작성자 정보 관리
+  const [reviewFrom, setReviewFrom] = useState({});
 
-  // 로그인 관리(추후 삭제 예정)
-  const loginHandler = () => {
-    setIsWriteReview((prev) => !prev);
-  };
+  // 로그인 정보를 확인
+  useEffect(() => {
+    console.log(loginInfo);
+    console.log(setLoginInfo);
+    if (!isLogin) {
+      alert('로그인을 해 주세요');
+      navigate('/home');
+    }
+  }, [setIsLogin]);
+
+  useEffect(() => {
+    axios
+      .get('/posts/1', {
+        headers: {
+          'Content-Type': `application/json`,
+          'ngrok-skip-browser-warning': '69420',
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setReviewData(res.data);
+        setReviewFrom(res.data.writerResponse);
+      });
+  }, []);
 
   // 버튼 클릭시 좋아요 넣기
   const likeHandler = () => {
     setLike((prev) => !prev);
-  };
-
-  //삭제하기
-  const deleteReviewHandler = () => {
-    const deleteReview = confirm('리뷰를 삭제하시겠습니까?');
-    if (deleteReview) {
-      alert('리뷰가 삭제되었습니다.');
-      navigate('/review');
-    }
   };
 
   // 모달창 관리하기
@@ -52,27 +74,25 @@ const ReviewDetail = () => {
           setReportModal={setReportModal}
         />
       )}
-      <button onClick={loginHandler}>로그인 관리</button>
       <SReviewDetailBlock>
         <SReviewHeader className="review-header">
           <img src="/images/Swear.png" alt="사진" />
           <SReviewUserInfo className="review-info">
-            <span>ZI존승민</span>
-            <span>2023-03-11 12:30</span>
+            <span>{reviewFrom.displayName}</span>
+            <span>{reviewData.modifiedAt}</span>
           </SReviewUserInfo>
         </SReviewHeader>
         <SReviewHospitalInfo className="hopital-info">
-          <span>승민병원</span>
+          <span>{reviewData.medicalTagTitle}</span>
+          {/* 추 후에 서버작업 완료되면 수정 예정 */}
           <span>⭐⭐⭐⭐⭐ (5.0) 점</span>
         </SReviewHospitalInfo>
         <SReviewContent className="contents">
-          <p>리뷰내용입니다. 어쩌구 저쩌구 그래서 정말 좋았어요~</p>
+          <p>{reviewData.content}~</p>
           <SReviewButtonBlock className="review-footer">
             <button onClick={likeHandler}>{like ? '❤️ 99' : '🖤'}</button>
-            {isWriteReview ? (
-              <div className="review-button">
-                <button onClick={deleteReviewHandler}>삭 제</button>
-              </div>
+            {loginInfo.memberId === reviewFrom.memberId ? (
+              <></>
             ) : (
               <div className="review-button">
                 <button onClick={reportModalHandler}>신 고</button>
@@ -81,6 +101,7 @@ const ReviewDetail = () => {
           </SReviewButtonBlock>
         </SReviewContent>
       </SReviewDetailBlock>
+      <CommentForm></CommentForm>
     </SReviewDetailContainer>
   );
 };
