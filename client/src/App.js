75aaec0607330a0db24058f84d75cf1414436a60
Mia-Oh <songmiaoh@gmail.com>
2023-03-18 21:00:08 +0900

    refactor:React.lazy 함수와 Suspense 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
 import Header from './Components/Header/Header';
-import Landing from './Pages/Landing/Landing';
-import Section from './Components/Section/Section';
-import Login from './Pages/Login/Login';
-import Signup from './Pages/Signup/Signup';
-import DoctorSignup from './Pages/DoctorSignup/DoctorSignup';
-import UserInfo from './Components/UserInfo/UserInfo';
-import AskQuestion from './Pages/AskQuestion/AskQuestion';
-import Review from './Pages/Review/Review';
 import Footer from './Components/Footer/Footer';
+
+const Loading = lazy(() => import('./Components/Loading/Loading'));
+const Landing = lazy(() => import('./Pages/Landing/Landing'));
+const Section = lazy(() => import('./Components/Section/Section'));
+const Login = lazy(() => import('./Pages/Login/Login'));
+const Signup = lazy(() => import('./Pages/Signup/Signup'));
+const DoctorSignup = lazy(() => import('./Pages/DoctorSignup/DoctorSignup'));
+const UserInfo = lazy(() => import('./Components/UserInfo/UserInfo'));
+const AskQuestion = lazy(() => import('./Pages/AskQuestion/AskQuestion'));
+const Review = lazy(() => import('./Pages/Review/Review'));
+const ReviewDetail = lazy(() => import('./Pages/ReviewDetail/ReviewDetail'));
+const QuestionDetail = lazy(() =>
+  import('./Pages/QuestionDetail/QuestionDetail')
+);
 
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
-import ReviewDetail from './Pages/ReviewDetail/ReviewDetail';
-import QuestionDetail from './Pages/QuestionDetail/QuestionDetail';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -24,18 +30,20 @@ function App() {
     <BrowserRouter>
       <GlobalStyle />
       <Header />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Section />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/medicalprovider" element={<DoctorSignup />} />
-        <Route path="/myinfo" element={<UserInfo />} />
-        <Route path="/askquestion" element={<AskQuestion />} />
-        <Route path="/review" element={<Review />} />
-        <Route path="/question/1234" element={<QuestionDetail />} />
-        <Route path="/review/1234" element={<ReviewDetail />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Section />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/medicalprovider" element={<DoctorSignup />} />
+          <Route path="/myinfo" element={<UserInfo />} />
+          <Route path="/askquestion" element={<AskQuestion />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/question/1234" element={<QuestionDetail />} />
+          <Route path="/review/1234" element={<ReviewDetail />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </BrowserRouter>
   );
