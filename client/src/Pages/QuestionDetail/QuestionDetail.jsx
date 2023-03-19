7dcd9758c2e8187715e87b29e17b6927a77e38e5
suming <ipower_overi@naver.com>
2023-03-19 21:19:42 +0900

    feat: 질문게시판 내 댓글 컴포넌트 및 style 구현 #132

--- client/src/Pages/QuestionDetail/QuestionDetail.jsx
+++ client/src/Pages/QuestionDetail/QuestionDetail.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { loginState, loggedUserInfo } from '../../atoms/atoms';
+import CommentForm from '../../Components/CommentForm/CommentForm';
+import Answers from '../../Components/Answers/Answers';
 
 import {
   SQuestionDetailContainer,
@@ -11,13 +13,8 @@ import {
   SQuestionInfoBlock,
   SQuestionTextBlock,
   SQuestionButtonBlock,
-  SAnswerHeader,
   SAnswerProfilePic,
   SPostAnswerBlock,
-  SAnswerBlock,
-  SAnswerInfoBlock,
-  SAnswerUserInfoBlock,
-  SAnswerButtonBlock,
   SQuestionLikeButtonBlock,
 } from '../../Style/QuestionDetailStyle';
 
@@ -31,16 +28,16 @@ const QuestionDetail = () => {
   // 글을 삭제할 경우 삭제 후 다른 페이지로 이동하기 위해
   const navigate = useNavigate();
   // 현재 로그인 상태가 글 작성자일 경우
-  // 글의 채택여부(전문가)
-  const [expertChoice, setExpertChoice] = useState(false);
-  // 글의 채택여부 (일반인)
-  const [normalChoice, setNormalChoice] = useState(false);
   // 질문 데이터
   const [questionData, setQuestionData] = useState({});
   // 질문 작성자 정보
   const [writerInfo, setWriterInfo] = useState({});
   // 답글 목록
   const [comments, setComments] = useState([]);
+  // 답글 작성자
+  const [commentFrom, setCommentFrom] = useState({});
+  // 답변창 다루기
+  const [postComment, setPostComment] = useState(false);
 
   useEffect(() => {
     // 로그인 상태가 아닐경우
@@ -55,7 +52,7 @@ const QuestionDetail = () => {
 
   useEffect(() => {
     axios
-      .get('/posts/5', {
+      .get('/posts/2', {
         headers: {
           'Content-Type': `application/json`,
           'ngrok-skip-browser-warning': '69420',
@@ -66,52 +63,23 @@ const QuestionDetail = () => {
         setQuestionData(res.data);
         setWriterInfo(res.data.writerResponse);
         setComments(res.data.comments);
+        setCommentFrom(res.data.comments.writerResponse);
       });
-  }, [questionData.totalLike]);
-  const expertChoiceHandler = () => {
-    if (!expertChoice) {
-      const expertChoiceConfirm = confirm('답변을 채택하시겠습니까?');
-      if (expertChoiceConfirm) {
-        alert('채택하였습니다.');
-        setExpertChoice((prev) => !prev);
-        setNormalChoice(false);
-      }
-    }
+  }, []);
 
-    if (expertChoice) {
-      const expertChoiceConfirmCancel =
-        confirm('답변 채택을 취소하시겠습니까?');
-      if (expertChoiceConfirmCancel) {
-        alert('채택을 취소하였습니다.');
-        setExpertChoice((prev) => !prev);
-      }
-    }
-  };
-
-  const normalChoiceHandler = () => {
-    if (!normalChoice) {
-      const normalChoiceConfirm = confirm('답변을 채택하시겠습니까?');
-      if (normalChoiceConfirm) {
-        alert('채택하였습니다.');
-        setNormalChoice((prev) => !prev);
-        setExpertChoice(false);
-      }
-    }
-
-    if (normalChoice) {
-      const expertChoiceCancel = confirm('답변 채택을 취소하시겠습니까?');
-      if (expertChoiceCancel) {
-        alert('채택을 취소하였습니다.');
-        setNormalChoice((prev) => !prev);
-      }
-    }
-  };
+  console.log('작성자');
+  console.log(comments);
+  console.log(commentFrom);
 
   const modifyHandler = () => {
     const modifyResult = confirm('질문을 수정하시겠습니까???');
     if (modifyResult) {
-      navigate('/askquestion');
+      navigate('/editquestion/2');
     }
+  };
+
+  const postCommentHandler = () => {
+    setPostComment((prev) => !prev);
   };
 
   const deleteHandler = () => {
@@ -141,13 +109,9 @@ const QuestionDetail = () => {
       })
       .then((res) => {
         location.reload();
-
         console.log(res);
       });
   };
-
-  console.log(comments);
-  console.log(questionData);
 
   return (
     <SQuestionDetailContainer className="detail-block">
@@ -176,81 +140,25 @@ const QuestionDetail = () => {
           </SQuestionLikeButtonBlock>
         )}
       </SQuestionDetailBlock>
-      {isLogin ? null : (
+      {userInfo[0].memberId === writerInfo.memberId ? null : (
         <SPostAnswerBlock className="want-answer-block">
           <SAnswerProfilePic src="/images/Swear.png" alt="img" />
           <div className="want-answer-text">
             <h1>대현자님의 답변을 기다리고 있어요!</h1>
             <span>지금 답변하여 채택받으시면 15점을 얻습니다.</span>
           </div>
-          <button>답변하기!</button>
+          <button onClick={postCommentHandler}>답변하기!</button>
         </SPostAnswerBlock>
       )}
+      {postComment ? <CommentForm /> : <></>}
       {/*  답글 여부에따라서 내용 변경, 서버가 완성되면 수정하겠음 */}
-      {/* {comments.length === 0 ? <></> : <></>} */}
-      <SAnswerBlock
-        className={
-          expertChoice ? 'expoert-choiced expert-answer' : 'expert-answer'
-        }
-      >
-        {expertChoice ? <span>채택된 답변</span> : null}
-        <SAnswerHeader className="header">
-          <h1>전문가 답변</h1>
-          {userInfo[0].memberId ? null : (
-            <div>
-              <button>수정</button>
-              <button>삭제</button>
-            </div>
-          )}
-        </SAnswerHeader>
-        <SAnswerInfoBlock className="answer-header-block">
-          <SAnswerUserInfoBlock className="answer-user-info">
-            <span>작성자</span>
-            <span>2023-03-02</span>
-          </SAnswerUserInfoBlock>
-          <SAnswerProfilePic src="/images/Swear.png" alt="img" />
-        </SAnswerInfoBlock>
-        <div className="answer-contents-block">
-          <p>답변 내용입니다. 제 생각은 이렇습니다. 어쩌구 저쩌구~</p>
-        </div>
-        <SAnswerButtonBlock className="answer-button-block">
-          <button onClick={expertChoiceHandler}>
-            {expertChoice ? '채택 취소' : '채택 하기'}
-          </button>
-        </SAnswerButtonBlock>
-      </SAnswerBlock>
-      <SAnswerBlock
-        className={
-          normalChoice ? 'normal-choiced normal-answer' : 'normal-answer'
-        }
-      >
-        {normalChoice ? <span>채택된 답변</span> : null}
-        <SAnswerHeader className="header">
-          <h1>일반인 답변</h1>
-          {isLogin ? null : (
-            <div>
-              <button>수정</button>
-              <button>삭제</button>
-            </div>
-          )}
-        </SAnswerHeader>
-        <SAnswerInfoBlock className="answer-header-block">
-          <SAnswerUserInfoBlock className="answer-user-info">
-            <span>작성자</span>
-            <span>2023-03-02</span>
-          </SAnswerUserInfoBlock>
-          <SAnswerProfilePic src="/images/Swear.png" alt="img" />
-        </SAnswerInfoBlock>
-        <div className="answer-contents-block">
-          <p>답변 내용입니다. 제 생각은 이렇습니다. 어쩌구 저쩌구~</p>
-        </div>
-        <SAnswerButtonBlock className="answer-button-block">
-          {/* 추후에 채택 취소 없애기 */}
-          <button onClick={normalChoiceHandler}>
-            {normalChoice ? '채택 취소' : '채택 하기'}
-          </button>
-        </SAnswerButtonBlock>
-      </SAnswerBlock>
+      {comments.length === 0 ? (
+        <></>
+      ) : (
+        comments.map((ele) => {
+          return <Answers ele={ele} key={ele.commentId} userInfo={userInfo} />;
+        })
+      )}
     </SQuestionDetailContainer>
   );
 };
