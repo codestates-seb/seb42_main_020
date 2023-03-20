e142f95904fd0f44f5ebd9ca2a40f740b97b5c09
suming <ipower_overi@naver.com>
2023-03-20 17:56:22 +0900

    style: 댓글작성 컴포넌트 내 스타일 코드 삭제

--- client/src/Components/CommentForm/CommentForm.jsx
+++ client/src/Components/CommentForm/CommentForm.jsx
@@ -1,67 +1,17 @@
-import styled from 'styled-components';
 import ReactQuill from 'react-quill';
 import axios from 'axios';
 import { useState } from 'react';
 import 'react-quill/dist/quill.bubble.css';
 
-const SLayout = styled.article`
-  font-weight: 600;
-  width: 790px;
-  margin: 30px 0 30px 0;
-  padding: 30px 10px;
-`;
-
-const SCommentOfferText = styled.h1`
-  width: 100px;
-  padding: 10px 10px 0 10px;
-  background-color: var(--gray-100);
-  text-align: center;
-  font-size: 15px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-`;
-
-const SwritingSection = styled.div`
-  padding: 15px;
-  background-color: var(--gray-100);
-  border-top-right-radius: 10px;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  box-shadow: 3px 2px 3px 2px var(--gray-200);
-`;
-
-const SWritingForm = styled.div`
-  border: 1px solid var(--gray-300);
-  box-shadow: 0 2px 3px 0 var(--gray-200);
-  height: 150px;
-  border-radius: 10px;
-`;
-
-const SBtnSection = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 10px 0 0 0;
-`;
-
-const SResetBtn = styled.button`
-  border: none;
-  background-color: var(--blue-500);
-  color: var(--white);
-  font-weight: 600;
-  border-radius: 10px;
-  padding: 7px 35px;
-  margin-right: 15px;
-  box-shadow: 0 2px 3px 0 var(--gray-200);
-  cursor: pointer;
-  :hover {
-    background-color: var(--blue-600);
-  }
-`;
-
-const SSubmitBtn = styled(SResetBtn)`
-  margin-right: 0px;
-`;
+import {
+  SLayout,
+  SCommentOfferText,
+  SwritingSection,
+  SWritingForm,
+  SBtnSection,
+  SResetBtn,
+  SSubmitBtn,
+} from '../../Style/CommentFormStyle';
 
 const CommentForm = () => {
   const token = localStorage.getItem('accessToken');
