aab50afd03c725dc3b38f162d679f3c949ba2483
Hoodie1368 <jhj5375@gmail.com>
2023-03-20 17:37:49 +0900

    [BE] feat : 댓글 수정, 댓글신고 등록  dto 기본생성자 추가

--- server/src/main/java/com/mainproject/comment/dto/CommentPatchDto.java
+++ server/src/main/java/com/mainproject/comment/dto/CommentPatchDto.java
@@ -2,12 +2,16 @@ package com.mainproject.comment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CommentPatchDto {
 
--- server/src/main/java/com/mainproject/postReport/dto/PostReportPostDto.java
+++ server/src/main/java/com/mainproject/postReport/dto/PostReportPostDto.java
@@ -2,12 +2,14 @@ package com.mainproject.postReport.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostReportPostDto {
 
