15b9be1415df9a7daeeb986eb7ac62773cadcd54
suming <ipower_overi@naver.com>
2023-03-20 17:57:15 +0900

    feat: 질문 답변 내 좋아요, 삭제, 수정, 채택 기능 구현 #132

--- client/src/Components/Answers/Answers.jsx
+++ client/src/Components/Answers/Answers.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { useState } from 'react';
+import EditCommentForm from '../../Components/CommentForm/EditCommentForm';
 
-import ReportModal from '../ReportModal/ReportModal';
+import ReportCommentModal from '../ReportModal/ReportComment';
 
 import {
   SAnswerHeader,
@@ -12,13 +13,23 @@ import {
   SAnswerButtonBlock,
 } from '../../Style/QuestionDetailStyle';
 
+import { SButtonBlock } from '../../Style/Answer';
+
 const Answers = ({ ele, userInfo }) => {
+  const token = localStorage.getItem('accessToken');
+  //수정 모달 관리
+  const [openEdit, setOpenEdit] = useState(false);
+
   // 신고 모달 관리
   const [reportModal, setReportModal] = useState(true);
   // 글의 채택여부(전문가)
   const [expertChoice, setExpertChoice] = useState(false);
   // 글의 채택여부 (일반인)
   //   const [normalChoice, setNormalChoice] = useState(false);
+
+  // 댓글 작성자 관리
+  const commentFrom = ele.writerResponse;
+  console.log(commentFrom);
 
   const expertChoiceHandler = () => {
     if (!expertChoice) {
@@ -59,10 +70,44 @@ const Answers = ({ ele, userInfo }) => {
   //     }
   //   };
 
-  const commentLikeHandler = (id) => {
-    axios.post(`/comments/${id}/likes`).then((res) => {
-      console.log(res);
+  // 댓글 삭제
+  const deleteHandler = () => {
+    axios({
+      method: 'delete',
+      url: `/comments/${ele.commentId}`,
+      headers: { Authorization: token },
+    }).then((res) => {
       location.reload();
+      console.log(res);
+    });
+  };
+
+  // 댓글 채택
+  const choiceHandler = () => {
+    axios({
+      method: 'patch',
+      url: `/posts/2/comments/${ele.commentId}`,
+      headers: { Authorization: token },
+    }).then((res) => {
+      location.reload();
+      console.log(res);
+    });
+  };
+
+  // 댓글 수정
+  const editCommentHandler = () => {
+    setOpenEdit((prev) => !prev);
+  };
+
+  // 좋아요
+  const commentLikeHandler = (id) => {
+    axios({
+      method: 'post',
+      url: `/comments/${id}/likes`,
+      headers: { Authorization: token },
+    }).then((res) => {
+      location.reload();
+      console.log(res);
     });
   };
 
@@ -76,31 +121,27 @@ const Answers = ({ ele, userInfo }) => {
       {reportModal ? (
         <></>
       ) : (
-        <ReportModal
+        <ReportCommentModal
           setReportModal={setReportModal}
           reportModalHandler={reportModalHandler}
+          ele={ele}
         />
       )}
 
       <SAnswerBlock
         className={
-          expertChoice ? 'expoert-choiced expert-answer' : 'expert-answer'
+          commentFrom.isDoctor
+            ? 'expoert-choiced expert-answer'
+            : 'expert-answer'
         }
       >
         {expertChoice ? <span>채택된 답변</span> : null}
         <SAnswerHeader className="header">
-          {ele.commentId === 1 ? <h1>전문가 답변</h1> : <h1>일반인 답변</h1>}
-          {/* 댓글 작성자 정보 확인되면 수정 */}
-          {userInfo[0].memberId ? null : (
-            <div>
-              <button>수정</button>
-              <button>삭제</button>
-            </div>
-          )}
+          {commentFrom.isDoctor ? <h1>전문가 답변</h1> : <h1>일반인 답변</h1>}
         </SAnswerHeader>
         <SAnswerInfoBlock className="answer-header-block">
           <SAnswerUserInfoBlock className="answer-user-info">
-            <span>작성자</span>
+            <span>{ele.displayName}</span>
             <span>{ele.createdAt}</span>
           </SAnswerUserInfoBlock>
           <SAnswerProfilePic src="/images/Swear.png" alt="img" />
@@ -116,16 +157,28 @@ const Answers = ({ ele, userInfo }) => {
           >
             ❤️ {ele.totalLike}
           </button>
-          <div>
-            <button onClick={reportModalHandler}>신고하기</button>
-            {/* 리뷰 작성자 리스트 나오면 해결 */}
-            {<button>삭제하기</button>}
-            <button onClick={expertChoiceHandler}>
-              {expertChoice ? '채택 취소' : '채택 하기'}
-            </button>
-          </div>
+          {userInfo[0].memberId === commentFrom.memberId ? (
+            <SButtonBlock>
+              <button onClick={deleteHandler}>삭제하기</button>
+              <button onClick={editCommentHandler}>수정</button>
+            </SButtonBlock>
+          ) : (
+            <div>
+              <button onClick={reportModalHandler}>신고하기</button>
+              <button onClick={choiceHandler}>
+                {expertChoice ? '채택 취소' : '채택 하기'}
+              </button>
+              <button onClick={expertChoiceHandler}>채택</button>
+            </div>
+          )}
+          <div></div>
         </SAnswerButtonBlock>
       </SAnswerBlock>
+      {openEdit ? (
+        <EditCommentForm commentId={ele.commentId} value={ele.content} />
+      ) : (
+        <></>
+      )}
     </>
   );
 };
