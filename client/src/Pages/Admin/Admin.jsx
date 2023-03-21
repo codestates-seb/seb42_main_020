76013e5e1f05e8fad869ce313a5e9c900db9438c
Mia-Oh <songmiaoh@gmail.com>
2023-03-22 00:38:32 +0900

    feat:관리자 페이지-승인 대기 목록 불러오기, 변한 로직에 따라 승인해주기 구현 #157

--- client/src/Pages/Admin/Admin.jsx
+++ client/src/Pages/Admin/Admin.jsx
@@ -1,100 +1,33 @@
 import axios from 'axios';
 import { useState } from 'react';
-import styled from 'styled-components';
-
-const SMain = styled.main`
-  width: 1920px;
-  height: 1000px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  background-color: var(--blue-50);
-`;
-
-const SLayout = styled.div`
-  width: 1500px;
-  height: 800px;
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  background-color: var(--white);
-  box-shadow: 0 10px 3px 0 var(--gray-200);
-  border-radius: 30px;
-  padding: 50px;
-`;
-
-const STitle = styled.h1`
-  font-family: 'TheJamsil5Bold';
-  font-size: 32px;
-  color: var(--gray-800);
-  margin-bottom: 10px;
-`;
-
-const SContent = styled.div`
-  height: 200px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const SSubTitle = styled.h2`
-  font-family: 'TheJamsil5Bold';
-  font-size: 25px;
-  margin-bottom: 10px;
-  color: var(--gray-800);
-  margin-bottom: 13px;
-`;
-
-const SSubText = styled.h3`
-  font-size: 17px;
-  margin-bottom: 13px;
-`;
-
-const SInput = styled.input`
-  width: 200px;
-  height: 40px;
-  margin: 10px 10px 10px 0;
-  border: 1px solid var(--gray-300);
-  border-radius: 5px;
-  box-shadow: 0 1px 3px 0 var(--gray-200);
-  font-size: 16px;
-  &:focus {
-    outline: none;
-  }
-`;
-
-const SSubmitBtn = styled.button`
-  width: 120px;
-  height: 40px;
-  margin: 10px 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--gray-200);
-  border: 1px solid var(--gray-200);
-  box-shadow: 0 1px 3px 0 var(--gray-200);
-  border-radius: 3px;
-  font-weight: 500;
-  font-size: 16px;
-  cursor: pointer;
-  :hover {
-    background-color: var(--blue-300);
-    border: 1px solid var(--blue-300);
-  }
-`;
-
-const SForm = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
+import PendingMemberItem from '../../Components/Admin/PendingMemberItem';
+import PendingReviewItem from '../../Components/Admin/PendingReviewItem';
+import {
+  SMain,
+  SLayout,
+  STitle,
+  SContent,
+  SSubTitle,
+  SSubText,
+  SInput,
+  SSubmitBtn,
+  SCallBtn,
+  SForm,
+  SItemListSection,
+  SContentSection,
+  SCallBtnSection,
+} from '../../Style/AdminStyle';
 
 const Admin = () => {
   const [memberId, setMemberId] = useState('');
   const [postId, setPostId] = useState('');
-  const [isDoneForDoc, setIsDoneForDoc] = useState(false);
-  const [isDoneForPost, setIsDonePost] = useState(false);
+
+  const [hospitalName, setHospitalName] = useState('');
+  const [hospitalContact, setHospitalContact] = useState('');
+  const [hopitalAddress, setHospitalAddress] = useState('');
+
+  const [pendingReviewList, setPendingReviewList] = useState({});
+  const [pendingMemberList, setPendingMemberList] = useState({});
 
   const handleChangeDocRegi = (e) => {
     setMemberId(e.target.value);
@@ -102,6 +35,19 @@ const Admin = () => {
 
   const handleChangePost = (e) => {
     setPostId(e.target.value);
+  };
+
+  const handleChangeHospitalName = (e) => {
+    setHospitalName(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleChangeHospitalContact = (e) => {
+    setHospitalContact(e.target.value);
+  };
+
+  const handleChangeHospitalAddress = (e) => {
+    setHospitalAddress(e.target.value);
   };
 
   const handleDocRegiSubmit = async () => {
@@ -113,7 +59,6 @@ const Admin = () => {
       console.log(res);
       if (res.status === 200) {
         // 승인 성공
-        setIsDoneForDoc(!isDoneForDoc);
       }
     } catch (error) {
       console.log('Error!', error);
@@ -128,35 +73,166 @@ const Admin = () => {
       });
       if (res.status === 200) {
         // 승인 성공
-        setIsDonePost(!isDoneForPost);
       }
     } catch (error) {
       console.log('Error!', error);
     }
   };
 
+  const handleHospitalRegiSubmit = async () => {
+    try {
+      const res = await axios({
+        method: 'post',
+        url: `/admin/hospital`,
+        data: {
+          name: hospitalName,
+          phone: hospitalContact,
+          address: hopitalAddress,
+        },
+      });
+      if (res.status === 200) {
+        // 승인 성공
+      }
+    } catch (error) {
+      console.log('Error!', error);
+    }
+  };
+
+  // /admin/members/8?hospitalName=율제병원
+  const handleDocRegiToHospitalSubmit = async () => {
+    try {
+      const res = await axios({
+        method: 'patch',
+        url: `/admin/members/${memberId}?hospitalName=${hospitalName}`,
+      });
+      if (res.status === 200) {
+        // 승인 성공
+      }
+    } catch (error) {
+      console.log('Error!', error);
+    }
+  };
+
+  // /admin/posts/5?hospitalName=다나아정형외과
+  const handlePostRegiToHospitalSubmit = async () => {
+    try {
+      const res = await axios({
+        method: 'patch',
+        url: `/admin/posts/${postId}?hospitalName=${hospitalName}`,
+      });
+      if (res.status === 200) {
+        // 승인 성공
+      }
+    } catch (error) {
+      console.log('Error!', error);
+    }
+  };
+
+  const handleClickMemLsit = () => {
+    axios({
+      method: 'get',
+      url: '/admin',
+      headers: {
+        'ngrok-skip-browser-warning': '69420',
+      },
+    }).then((res) => {
+      setPendingReviewList(res.data);
+      setPendingMemberList(res.data);
+    });
+  };
+
   return (
     <SMain>
       <SLayout>
         <STitle>관리자 승인 대기</STitle>
-        <SContent>
-          <SSubTitle>의사 회원가입 승인</SSubTitle>
-          <SSubText>승인 대기 중인 멤버의 ID를 입력해 주세요</SSubText>
-          <SForm>
-            <SInput onChange={handleChangeDocRegi} placeholder="member-id" />
-            <SSubmitBtn onClick={handleDocRegiSubmit}>승인</SSubmitBtn>
-          </SForm>
-          {isDoneForDoc ? <SSubText>승인이 완료되었습니다.</SSubText> : <></>}
-        </SContent>
-        <SContent>
-          <SSubTitle>리뷰 포스트 등록 승인</SSubTitle>
-          <SSubText>승인 대기 중인 리뷰 게시물의 ID를 입력해 주세요</SSubText>
-          <SForm>
-            <SInput onChange={handleChangePost} placeholder="post-id" />
-            <SSubmitBtn onClick={handleReviewPostSubmit}>승인</SSubmitBtn>
-          </SForm>
-          {isDoneForPost ? <SSubText>승인이 완료되었습니다.</SSubText> : <></>}
-        </SContent>
+        <SContentSection>
+          <div>
+            <SContent>
+              <SSubTitle>병원 등록</SSubTitle>
+              <SSubText>
+                등록 대기 중인 병원의 병원명, 연락처, 주소를 입력해 주세요
+              </SSubText>
+              <SForm>
+                <SInput
+                  onChange={handleChangeHospitalName}
+                  placeholder="병원명"
+                />
+                <SInput
+                  onChange={handleChangeHospitalContact}
+                  placeholder="연락처"
+                />
+                <SInput
+                  onChange={handleChangeHospitalAddress}
+                  placeholder="주소"
+                />
+              </SForm>
+              <SSubmitBtn onClick={handleHospitalRegiSubmit}>승인</SSubmitBtn>
+            </SContent>
+            <SContent>
+              <SSubTitle>병원에 의사 회원 등록</SSubTitle>
+              <SSubText>등록되어 있는 병원에 의사 회원을 등록합니다.</SSubText>
+              <SForm>
+                <SInput
+                  onChange={handleChangeHospitalName}
+                  placeholder="병원명"
+                />
+                <SInput
+                  onChange={handleChangeDocRegi}
+                  placeholder="의사 회원 Id"
+                />
+              </SForm>
+              <SSubmitBtn onClick={handleDocRegiToHospitalSubmit}>
+                승인
+              </SSubmitBtn>
+            </SContent>
+            <SContent>
+              <SSubTitle>병원에 리뷰 게시글 등록</SSubTitle>
+              <SSubText>
+                등록되어 있는 병원에 리뷰 게시글을 등록합니다.
+              </SSubText>
+              <SForm>
+                <SInput
+                  onChange={handleChangeHospitalName}
+                  placeholder="병원명"
+                />
+                <SInput onChange={handleChangePost} placeholder="post-id" />
+              </SForm>
+              <SSubmitBtn onClick={handlePostRegiToHospitalSubmit}>
+                승인
+              </SSubmitBtn>
+            </SContent>
+            <SContent>
+              <SSubTitle>의사 회원가입 승인</SSubTitle>
+              <SSubText>승인 대기 중인 멤버의 ID를 입력해 주세요</SSubText>
+              <SForm>
+                <SInput
+                  onChange={handleChangeDocRegi}
+                  placeholder="member-id"
+                />
+              </SForm>
+              <SSubmitBtn onClick={handleDocRegiSubmit}>승인</SSubmitBtn>
+            </SContent>
+            <SContent>
+              <SSubTitle>리뷰 포스트 등록 승인</SSubTitle>
+              <SSubText>
+                승인 대기 중인 리뷰 게시물의 ID를 입력해 주세요
+              </SSubText>
+              <SForm>
+                <SInput onChange={handleChangePost} placeholder="post-id" />
+              </SForm>
+              <SSubmitBtn onClick={handleReviewPostSubmit}>승인</SSubmitBtn>
+            </SContent>
+          </div>
+          <SItemListSection>
+            <SCallBtnSection>
+              <SCallBtn onClick={handleClickMemLsit}>
+                승인 대기 리스트 불러오기
+              </SCallBtn>
+            </SCallBtnSection>
+            <PendingMemberItem pendingMemberList={pendingMemberList} />
+            <PendingReviewItem pendingReviewList={pendingReviewList} />
+          </SItemListSection>
+        </SContentSection>
       </SLayout>
     </SMain>
   );
