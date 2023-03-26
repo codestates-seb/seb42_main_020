6b63c411892d9ccd66ac0c279749e16592278dce
Mia-Oh <songmiaoh@gmail.com>
2023-03-26 12:16:08 +0900

    fix:getAccessTokenFromLocal hook 오류 수정

--- client/src/util/Token.js
+++ client/src/util/Token.js
@@ -18,23 +18,23 @@ export const setAccessTokenToLocal = (accessToken) => {
 export const getAccessTokenFromLocal = () => {
   useEffect(() => {
     const accessToken = localStorage.getItem('accessToken');
-    const refreshToken = cookies.get('refreshToken');
     axios
       .get('/auth/access', {
         headers: { Authorization: accessToken },
       })
-      .then(() => {
-        // if (
-        //   res?.status === 200 &&
-        //   res?.data === 'Access token validation successful!'
-        // ) {
-        //   console.log('엑세스 토큰 유효', res?.data);
-        // } else {
-        //   console.log('유효 검증 과정에서 에러 발생');
-        // }
+      .then((res) => {
+        if (
+          res?.status === 200 &&
+          res?.data === 'Access token validation successful!'
+        ) {
+          // console.log('엑세스 토큰 유효', res?.data);
+        } else {
+          // console.log('유효 검증 과정에서 에러 발생');
+        }
         //! 사용자에게 노출 X
       })
       .catch((error) => {
+        const refreshToken = cookies.get('refreshToken');
         if (
           error?.response?.status === 401 &&
           error?.response?.statusText === 'Unauthorized'
