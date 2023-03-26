d206347388436f5395465c920866ce0cb04d687c
Mia-Oh <songmiaoh@gmail.com>
2023-03-26 17:18:02 +0900

    fix:지역구, 과목별 내 페이지 이동 안되는 것 수정

--- client/src/Components/Nav/TypeButton.jsx
+++ client/src/Components/Nav/TypeButton.jsx
@@ -10,79 +10,79 @@ const TypeButton = () => {
     },
     {
       key: '2',
-      label: <Link to="/home/subject/2">안과</Link>,
+      label: <a href="/home/subject/2">안과</a>,
     },
     {
       key: '3',
-      label: <Link to="/home/subject/3">피부과</Link>,
+      label: <a href="/home/subject/3">피부과</a>,
     },
     {
       key: '4',
-      label: <Link to="/home/subject/4">성형외과</Link>,
+      label: <a href="/home/subject/4">성형외과</a>,
     },
     {
       key: '5',
-      label: <Link to="/home/subject/5">산부인과</Link>,
+      label: <a href="/home/subject/5">산부인과</a>,
     },
     {
       key: '6',
-      label: <Link to="/home/subject/6">정신건강의학과</Link>,
+      label: <a href="/home/subject/6">정신건강의학과</a>,
     },
     {
       key: '7',
-      label: <Link to="/home/subject/7">비뇨기과</Link>,
+      label: <a href="/home/subject/7">비뇨기과</a>,
     },
     {
       key: '8',
-      label: <Link to="/home/subject/8">정형외과</Link>,
+      label: <a href="/home/subject/8">정형외과</a>,
     },
     {
       key: '9',
-      label: <Link to="/home/subject/9">마취통증의학과</Link>,
+      label: <a href="/home/subject/9">마취통증의학과</a>,
     },
     {
       key: '10',
-      label: <Link to="/home/subject/10">신경외과</Link>,
+      label: <a href="/home/subject/10">신경외과</a>,
     },
     {
       key: '11',
-      label: <Link to="/home/subject/11">재활의학과</Link>,
+      label: <a href="/home/subject/11">재활의학과</a>,
     },
     {
       key: '12',
-      label: <Link to="/home/subject/12">영상의학과</Link>,
+      label: <a href="/home/subject/12">영상의학과</a>,
     },
     {
       key: '13',
-      label: <Link to="/home/subject/13">외과</Link>,
+      label: <a href="/home/subject/13">외과</a>,
     },
     {
       key: '14',
-      label: <Link to="/home/subject/14">신경과</Link>,
+      label: <a href="/home/subject/14">신경과</a>,
     },
     {
       key: '15',
-      label: <Link to="/home/subject/15">소아과</Link>,
+      label: <a href="/home/subject/15">소아과</a>,
     },
     {
       key: '16',
-      label: <Link to="/home/subject/16">내과</Link>,
+      label: <a href="/home/subject/16">내과</a>,
     },
     {
       key: '17',
-      label: <Link to="/home/subject/17">이빈후과</Link>,
+      label: <a href="/home/subject/17">이빈후과</a>,
     },
     {
       key: '18',
-      label: <Link to="/home/subject/18">가정의학과</Link>,
+      label: <a href="/home/subject/18">가정의학과</a>,
     },
     {
       key: '19',
-      label: <Link to="/home/subject/19">한의원</Link>,
+      label: <a href="/home/subject/19">한의원</a>,
     },
     {
       key: '20',
-      label: <Link to="/home/subject/20">기타</Link>,
+      label: <a href="/home/subject/20">기타</a>,
     },
   ];
 
@@ -94,7 +94,7 @@ const TypeButton = () => {
         }}
       >
         <SNavContents onClick={(e) => e.preventDefault()}>
-          <Link to="/home/subject">과목별</Link>
+          <Link to="/home/subject/1">과목별</Link>
         </SNavContents>
       </Dropdown>
     </>
--- client/src/Pages/home/LocationPosts.jsx
+++ client/src/Pages/home/LocationPosts.jsx
@@ -8,92 +8,92 @@ function LocationPosts() {
 export const items = [
   {
     key: '1',
-    label: <Link to="/home/location/1">강남구</Link>,
+    label: <Link to="home/location/1">강남구</Link>,
   },
   {
     key: '2',
-    label: <Link to="/home/location/2">강서구</Link>,
+    label: <a href="/home/location/2">강서구</a>,
   },
   {
     key: '3',
-    label: <Link to="/home/location/3">강북구</Link>,
+    label: <a href="/home/location/3">강북구</a>,
   },
   {
     key: '4',
-    label: <Link to="/home/location/4">용산구</Link>,
+    label: <a href="/home/location/4">용산구</a>,
   },
   {
     key: '5',
-    label: <Link to="/home/location/5">관악구</Link>,
+    label: <a href="/home/location/5">관악구</a>,
   },
   {
     key: '6',
-    label: <Link to="/home/location/6">중구</Link>,
+    label: <a href="/home/location/6">중구</a>,
   },
   {
     key: '7',
-    label: <Link to="/home/location/7">서초구</Link>,
+    label: <a href="/home/location/7">서초구</a>,
   },
   {
     key: '8',
-    label: <Link to="/home/location/8">송파구</Link>,
+    label: <a href="/home/location/8">송파구</a>,
   },
   {
     key: '9',
-    label: <Link to="/home/location/9">마포구</Link>,
+    label: <a href="/home/location/9">마포구</a>,
   },
   {
     key: '10',
-    label: <Link to="/home/location/10">서대문구</Link>,
+    label: <a href="/home/location/10">서대문구</a>,
   },
   {
     key: '11',
-    label: <Link to="/home/location/11">성동구</Link>,
+    label: <a href="/home/location/11">성동구</a>,
   },
   {
     key: '12',
-    label: <Link to="/home/location/12">은평구</Link>,
+    label: <a href="/home/location/12">은평구</a>,
   },
   {
     key: '13',
-    label: <Link to="/home/location/13">동대문구</Link>,
+    label: <a href="/home/location/13">동대문구</a>,
   },
   {
     key: '14',
-    label: <Link to="/home/location/14">동작구</Link>,
+    label: <a href="/home/location/14">동작구</a>,
   },
   {
     key: '15',
-    label: <Link to="/home/location/15">노원구</Link>,
+    label: <a href="/home/location/15">노원구</a>,
   },
   {
     key: '16',
-    label: <Link to="/home/location/16">도봉구</Link>,
+    label: <a href="/home/location/16">도봉구</a>,
   },
   {
     key: '17',
-    label: <Link to="/home/location/17">영등포구</Link>,
+    label: <a href="/home/location/17">영등포구</a>,
   },
   {
     key: '18',
-    label: <Link to="/home/location/18">중랑구</Link>,
+    label: <a href="/home/location/18">중랑구</a>,
   },
 
   {
     key: '19',
-    label: <Link to="/home/location/19">금천구</Link>,
+    label: <a href="/home/location/19">금천구</a>,
   },
   {
     key: '20',
-    label: <Link to="/home/location/20">광진구</Link>,
+    label: <a href="/home/location/20">광진구</a>,
   },
   {
     key: '21',
-    label: <Link to="/home/location/21">성북구</Link>,
+    label: <a href="/home/location/21">성북구</a>,
   },
   {
     key: '22',
-    label: <Link to="/home/location/22">구로구</Link>,
+    label: <a href="/home/location/22">구로구</a>,
   },
 ];
 
