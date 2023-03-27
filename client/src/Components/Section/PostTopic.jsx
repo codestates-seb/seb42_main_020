d55fbf1e2437f947da41fc921e9ac8c3cbf67335
Observant0120 <vjw21@naver.com>
2023-03-28 03:13:06 +0900

    remove: 게시글 정렬 순서 삭제

--- client/src/Components/Section/PostTopic.jsx
+++ client/src/Components/Section/PostTopic.jsx
@@ -1,43 +1,12 @@
 import PostTopicStlye from '../../Style/PostTopicStlye';
 import MakeContents from '../MakeContents/MakeContents';
-import { Select, Space } from 'antd';
 
 function PostTopic({ topicName }) {
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
-  };
-
   return (
     <PostTopicStlye>
       <div className="topicTemplate">
         <h2>{topicName} 게시판 ✍</h2>
-        <Space wrap style={{ marginBottom: -10 }}>
-          <Select
-            defaultValue="latest"
-            style={{ width: 120 }}
-            onChange={handleChange}
-            options={[
-              {
-                value: 'latest',
-                label: '최신순',
-              },
-              {
-                value: 'like',
-                label: '인기순',
-              },
-              {
-                value: 'doctor',
-                label: '전문의 답변',
-              },
-              {
-                value: 'disabled',
-                label: 'Disabled',
-                disabled: true,
-              },
-            ]}
-          />
-          <MakeContents className="create-post">글쓰기</MakeContents>
-        </Space>
+        <MakeContents className="create-post">글쓰기</MakeContents>
       </div>
     </PostTopicStlye>
   );
