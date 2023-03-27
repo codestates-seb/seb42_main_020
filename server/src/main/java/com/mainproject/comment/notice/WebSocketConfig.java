a5cfbd4a3c5474988f0636f80e0d45ea15d307b5
Hoodie1368 <jhj5375@gmail.com>
2023-03-28 01:46:28 +0900

    [BE] refactor : 웹소켓 설정

--- server/src/main/java/com/mainproject/comment/notice/WebSocketConfig.java
+++ server/src/main/java/com/mainproject/comment/notice/WebSocketConfig.java
@@ -13,12 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic", "queue");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/members").setAllowedOrigins("*").withSockJS();
     }
 }
